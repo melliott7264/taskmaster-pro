d63b1a5e87b96040dc310c651edaab5e53674213
Mark Elliott <96553641+melliott7264@users.noreply.github.com>
2022-03-22 18:41:50 -0400

    added some drag and drop functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -110,6 +110,28 @@ $(".list-group").on("blur", "input[type='text']", function() {
 
 });
 
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event) {
+    console.log("activate", this);
+  },
+  deactivate: function(event) {
+    console.log("deactivate", this);
+  },
+  over: function(event) {
+    console.log("over", event.target);
+  },
+  out: function(event) {
+    console.log("out", event.target);
+  },
+  update: function(event) {
+    console.log($(this).children());
+  }
+});
+
 
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function() {
