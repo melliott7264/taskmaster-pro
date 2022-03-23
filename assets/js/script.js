f06a72bc541a37253f8da734e1ecb75b206dd0df
Mark Elliott <96553641+melliott7264@users.noreply.github.com>
2022-03-23 11:05:14 -0400

    added date picker function

--- assets/js/script.js
+++ assets/js/script.js
@@ -82,13 +82,23 @@ $(".list-group").on("click", "span", function() {
   // swap out elements
   $(this).replaceWith(dateInput);
 
+  // enable Jquery UI datepicker
+  dateInput.datepicker({
+    minDate: 1,
+    onClose: function () {
+      // when calendar is closed, force a "change" event on the "dateInput"
+      $(this).trigger("change");
+    }
+
+  });
+
   // automatically focus on new element
   dateInput.trigger("focus");
 
 });
 
 //value of due date was changed
-$(".list-group").on("blur", "input[type='text']", function() {
+$(".list-group").on("change", "input[type='text']", function() {
   // get current text
   var date = $(this).val().trim();
 
@@ -176,6 +186,10 @@ $("#trash").droppable({
   out: function(event,ui) {
     console.log("out");
   }
+});
+
+$("#modalDueDate").datepicker({
+  minDate: 1
 });
 
 // modal was triggered
